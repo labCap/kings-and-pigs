95f19801ef067d086d4111bbe6694ba9681847b5
LCap <banlazthosow@gmail.com>
2022-10-29 22:07:35 +0300

    added entering door

--- js/eventListners.js
+++ js/eventListners.js
@@ -1,6 +1,25 @@
 window.addEventListener("keydown", (event) => {
+  if (player.preventInput) return;
   switch (event.key) {
     case "w":
+      for (let i = 0; i < doors.length; i++) {
+        const door = doors[i];
+
+        if (
+          player.hitBox.position.x + player.hitBox.width <=
+            door.position.x + door.width &&
+          player.hitBox.position.x >= door.position.x &&
+          player.hitBox.position.y + player.hitBox.height >= door.position.y &&
+          player.hitBox.position.y <= door.position.y + door.height
+        ) {
+          player.velocity.x = 0;
+          player.velocity.y = 0;
+          player.preventInput = true;
+          player.switchSprite("enterDoor");
+          door.play();
+          return;
+        }
+      }
       if (player.velocity.y === 0) {
         player.velocity.y = -20;
       }
