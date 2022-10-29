6a7157638c27613c9ef22ff2e42db91c79792431
LCap <banlazthosow@gmail.com>
2022-10-29 18:51:22 +0300

    added collision blocks

--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
+const parsedCollisions = collisionsLevel1.parse2D();
+
+const collisionBlocks = parsedCollisions.createObjectsFrom2D();
+
 const backgroundLevel1 = new Sprite({
   position: {
     x: 0,
@@ -25,6 +29,9 @@ function animate() {
   window.requestAnimationFrame(animate);
 
   backgroundLevel1.draw();
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.draw();
+  });
 
   player.velocity.x = 0;
   if (keys.d.pressed) {
