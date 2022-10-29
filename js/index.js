1e8fd2f77a4051a28ab1395e901dcf13b066781f
LCap <banlazthosow@gmail.com>
2022-10-29 17:04:07 +0300

    create a player

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = 64 * 16;
+canvas.height = 64 * 9;
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+const player = new Player();
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.draw();
+  player.update();
+}
+animate();
